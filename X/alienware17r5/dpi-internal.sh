ec503ee3aefcb348da8f8e8de814c7d4a4e15b0b
Martin Suchanek <mrtn@nrd.io>
2018-08-21 22:43:34 -0700

    Add alienware17r5 X configs and scripts

---
+++ X/alienware17r5/dpi-internal.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp ~/.Xresources.internal ~/.Xresources && cp ~/.xinitrc.internal ~/.xinitrc && kill -9 -1
---
+++ X/alienware17r5/monitor-both.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+xrandr --output "DP-0" --primary --auto --output "DP-1" --right-of "DP-0" --mode 3440x1440 --rate 100.00
